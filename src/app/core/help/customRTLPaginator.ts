840e30b11b25437bf3afa3aa4306c006439863eb
HanenDhafer <haneendhaferg@gmail.com>
2022-11-24 18:10:20 +0300

    edit paginator

--- src/app/core/help/customRTLPaginator.ts
+++ src/app/core/help/customRTLPaginator.ts
@@ -6,6 +6,15 @@ export function CustomPaginator() {
   customPaginatorIntl.itemsPerPageLabel = 'عدد الحقول';
   customPaginatorIntl.nextPageLabel="التالي"
   customPaginatorIntl.previousPageLabel="السابق"
+  customPaginatorIntl.getRangeLabel= (page: number, pageSize: number, length: number) =>  {
+    if (length === 0 || pageSize === 0) {
+      return `0 / ${length}`;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+    return `${startIndex + 1} - ${endIndex} من ${length}`;
+  }
   
   return customPaginatorIntl;
 }
